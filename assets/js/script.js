d322894aaa849db7a561f07415a6ce412cd62218
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2025-03-14 10:03:22 -0300

    see product

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,24 @@ function clikMobile() {
         menuItens.style.maxHeight = '0px';
     }
 
-    menuMobile.classList.toggle('active')    
+    menuMobile.classList.toggle('active') 
 }
+
+const productImg = document.getElementById('productImg')
+const miniatureProduct = document.getElementsByClassName('miniature-product')
+
+miniatureProduct[0].onclick = () => {
+    productImg.src = miniatureProduct[0].src
+}
+
+miniatureProduct[1].onclick = () => {
+    productImg.src = miniatureProduct[1].src
+}
+
+miniatureProduct[2].addEventListener('click', () => {
+    productImg.src = miniatureProduct[2].src
+})
+
+miniatureProduct[3].addEventListener('click', () => {
+    productImg.src = miniatureProduct[3].src
+})
