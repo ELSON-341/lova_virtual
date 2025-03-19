3a987877134c1a5e75357d11808af6190038f3df
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2025-03-19 10:19:04 -0300

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,18 +16,55 @@ function clikMobile() {
 const productImg = document.getElementById('productImg')
 const miniatureProduct = document.getElementsByClassName('miniature-product')
 
-miniatureProduct[0].onmouseover = () => {
-    productImg.src = miniatureProduct[0].src
-}
+// miniatureProduct[0].onmouseover = () => {
+//     productImg.src = miniatureProduct[0].src
+// }
 
-miniatureProduct[1].onmouseover = () => {
-    productImg.src = miniatureProduct[1].src
-}
+// miniatureProduct[1].onmouseover = () => {
+//     productImg.src = miniatureProduct[1].src
+// }
 
-miniatureProduct[2].addEventListener('mouseover', () => {
-    productImg.src = miniatureProduct[2].src
-})
+// miniatureProduct[2].addEventListener('mouseover', () => {
+//     productImg.src = miniatureProduct[2].src
+// })
 
-miniatureProduct[3].addEventListener('mouseover', () => {
-    productImg.src = miniatureProduct[3].src
-})
+// miniatureProduct[3].addEventListener('mouseover', () => {
+//     productImg.src = miniatureProduct[3].src
+// })
+
+$(document).ready(function () {
+    $("#productImg").elevateZoom({
+        // Inicializa o zoom
+        zoomType: "window",   // Tipo de zoom (pode ser "inner", "window", "lens")
+        lensShape: "square", // Formato da lente (round ou square)
+        lensSize: 150,       // Tamanho da lente
+        cursor: "crosshair"
+    });
+    
+    $("#productImg").elevateZoom(zoomConfing)
+
+     // Ao clicar na miniatura, mudar a imagem principal
+    $(".miniature-product").click(function () {
+        let newImage = $(this).attr("data-image")
+
+        let zoomInstance = $("#productImg").data("elevateZoom")
+        if(zoomInstance) {
+            zoomInstance.destroy();
+        }
+
+        // Atualiza a imagem principal e a imagem de zoom
+        $('#productImg').attr("src", newImage).attr("data-zoom-image", newImage);
+
+        $("#productImg").elevateZoom(zoomConfing)
+
+        // Remove o zoom anterior e aplica novamente
+        $(".zoomContainer").remove();
+        $("#productImg").removeData("elevateZoom");
+        $("#productImg").elevateZoom({
+            zoomType: "window",   // Tipo de zoom (pode ser "inner", "window", "lens")
+            lensShape: "square", // Formato da lente (round ou square)
+            lensSize: 150,       // Tamanho da lente
+            cursor: "crosshair"
+        })
+    })
+});
