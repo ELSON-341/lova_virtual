a98b18012477ef254c0ca3812444cab37c8ca17a
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2025-03-27 09:21:13 -0300

    login

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+const menuItens = document.querySelector('#menu-itens');
+const menuMobile = document.querySelector('.menu-mobile')
+
+menuItens.style.maxHeight = "0px";
+
+function clikMobile() {
+    if (menuItens.style.maxHeight === "0px") {
+        menuItens.style.maxHeight = '200px';
+    } else {
+        menuItens.style.maxHeight = '0px';
+    }
+
+    menuMobile.classList.toggle('active') 
+}
